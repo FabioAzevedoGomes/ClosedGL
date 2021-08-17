983848d4307728bbe6ab676e6e59e219c416da6e
Fábio Gomes <fabioazevedogomes@hotmail.com>
2021-08-17 19:01:08 -0300

    Remove build files

--- build/CMakeFiles/ClosedGL.dir/src/BufferUtils.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ClosedGL.dir/src/BufferUtils.cpp.obj: \
- C:\Users\Home\Desktop\CG\ClosedGL\src\BufferUtils.cpp \
- C:/Users/Home/Desktop/CG/ClosedGL/include/BufferUtils.hpp
--- build/CMakeFiles/ImGui.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "lib/libImGui_d.a"
-)
